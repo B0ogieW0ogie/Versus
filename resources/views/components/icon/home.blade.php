8be549f1a9c34c830c6940e5f1d615c26789e09a
mrcloud <mister.cloud@yandex.ru>
2026-04-22 19:07:19 +0300

    Add navigation icon components

---
+++ resources/views/components/icon/home.blade.php
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+     stroke-width="1.5" stroke="currentColor" {{ $attributes->merge(['class' => 'h-5 w-5']) }}>
+    <path stroke-linecap="round" stroke-linejoin="round"
+          d="M2.25 12 12 3.75 21.75 12M4.5 9.75v9.75h4.5v-6h6v6h4.5V9.75" />
+</svg>
