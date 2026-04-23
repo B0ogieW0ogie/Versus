70eb86cbb180cee453f02ad42473036c2806bbc0
mrcloud <mister.cloud@yandex.ru>
2026-04-23 13:11:08 +0300

    Add image-placeholder / plus / feed icons

---
+++ resources/views/components/icon/image-placeholder.blade.php
@@ -0,0 +1,7 @@
+<svg {{ $attributes->merge(['class' => 'h-6 w-6 text-white/30', 'fill' => 'none', 'stroke' => 'currentColor', 'viewBox' => '0 0 24 24']) }}
+     stroke-width="1.5" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <path stroke-linecap="round" stroke-linejoin="round"
+          d="M3.75 5.25a1.5 1.5 0 0 1 1.5-1.5h13.5a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V5.25Z" />
+    <path stroke-linecap="round" stroke-linejoin="round"
+          d="m3.75 16.5 4.5-4.5 3 3 4.5-4.5 4.5 4.5M9 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
+</svg>
